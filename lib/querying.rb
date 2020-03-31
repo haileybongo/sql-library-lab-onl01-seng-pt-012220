258ae2c9d05afaf921dc4fd3bdffc1e7dc1e7749
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 20:59:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,9 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM characters"
+  FROM series
+  INNER JOIN characters 
+  ON characters."
 end
 
 def select_character_names_and_number_of_books_they_are_in
