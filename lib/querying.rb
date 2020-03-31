c12dd87b0868080f575791a3f158dc9959a452b6
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 20:27:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
-  INNER JOIN series
-  ON books.series_id = series.id 
+
   WHERE book.series.id = 1
   SORT BY(books.year);"
 end
