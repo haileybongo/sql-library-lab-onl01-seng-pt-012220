23e5318316f86d03bf6405d11d7b34a4014118db
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 20:14:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year, books.series 
+  "SELECT books.title, books.year, series.name 
   FROM books
   INNER JOIN series
-  ON books.series"
+  ON books.series_id = series.id 
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
