7699a61057a44447338e5665253889061fa91c63
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 21:15:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,11 +29,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species)
+  "SELECT series.title
   FROM series
   INNER JOIN characters 
   ON series.author_id = characters.author_id
-  WHERE characters.species = 'human'
+  WHERE characters.species = "human"
   GROUP BY characters.species
   ORDER BY COUNT(characters.species) DESC LIMIT 1 ;"
 end
